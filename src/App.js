23919364768422d7144522a8a99cc6875a18b641
Mikael Jäntti <mikael.jantti@gmail.com>
2025-03-04 13:01:45 +0200

    app index changes for routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import Banner from './Components/Banner/Banner';
 import Gallery from './Components/Gallery/Gallery';
 import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import GalleryPage from './Pages/GalleryPage';
+import HomePage from './Pages/HomePage';
 
-function App() {
-  return (
-    <>
-        <Header/>
-        <Hero/>
-        <Banner/>
-        <Gallery/>
-    </>
-  );
-}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+  {
+    path: "/GalleryPage",
+    element: <GalleryPage/>
+  }
+])
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from './Pages/HomePage';
-import GalleryPage from './Pages/GalleryPage';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>
-  },
-  {
-    path: "/GalleryPage",
-    element: <GalleryPage/>
-  }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider  router={router}/>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
