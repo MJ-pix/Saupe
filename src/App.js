a058b5b13793b7832684a5be8450c2826123c7d6
Mikael Jäntti <mikael.jantti@gmail.com>
2025-03-04 13:29:35 +0200

    set link to gallerypage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Banner from './Components/Banner/Banner';
 import Gallery from './Components/Gallery/Gallery';
 import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
 import GalleryPage from './Pages/GalleryPage';
 import HomePage from './Pages/HomePage';
 
--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import "./ProjectCard.css"
 
   const ProjectCard = ({ imageSrc, title, href }) => {
       return (
           //VIDEON KORKEUDET STAATTISEKSI  TAI JOTAI JA OBJECTFIT COVER YMSMS
-          <div className="card"><a style={{textDecoration: "none", color:"black"}} href={href}>
+          <div className="card"><Link style={{textDecoration: "none", color:"black"}} to={href}>
             {/* <img src={imageSrc} alt={title} className="card-image" /> */}
             <div className="video-wrapper">
             <video width="100%" height="fit-content" loop muted autoPlay src={imageSrc} className='video'></video>
@@ -11,7 +12,7 @@ import "./ProjectCard.css"
             <div className="card-text">
               <h3>{title}</h3>
             </div>
-            </a>  
+            </Link>
           </div>
       );
     };
